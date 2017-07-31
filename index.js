d43ee9186f3811876c643498e431a4a690135a9c
4y0 <4y0d3jii@gmail.com>
2017-07-31 14:27:13 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var base     = require('./lib/rave.base'); 
+var card     = require('./lib/rave.card');
+var account  = require('./lib/rave.account');
+var status   = require('./lib/rave.status');
+var mobile   = require('./lib/rave.mobile');
+var misc     = require('./lib/rave.misc');
+
+
+var Rave = function (public_key, public_secret, base_url_or_production_flag)
+{
+
+	var ravebase = new base(public_key, public_secret, base_url_or_production_flag);
+
+	this.Card          = new card(ravebase);
+	this.Status        = new status(ravebase);
+	this.Account       = new account(ravebase);
+	this.MobileOptions = new mobile(ravebase);
+	this.Misc          = new misc(ravebase);
+
+	this.getIntegrityHash = function (data) { return ravebase.getIntegrityHash(data); }
+
+} 
+
+
+module.exports = Rave;
