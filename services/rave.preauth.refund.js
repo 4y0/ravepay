e397568413c051d79c259ea6b388072f883f3029
4y0 <4y0d3jii@gmail.com>
2017-11-13 15:58:36 +0100

    Ref to refund / void services

--- services/rave.preauth.refund.js
+++ services/rave.preauth.refund.js
@@ -3,7 +3,7 @@ var q = require('q');
 
 
 var spec = morx.spec()
-                .build('id', 'required:true,validators:isNumeric, eg:7345')
+                .build('id', 'required:false,validators:isNumeric, eg:7345')
                 .build('ref', 'required:false,validators:isNumeric, eg:7345')
 				.build('action', 'required:true, eg:refund')
                 .end();
